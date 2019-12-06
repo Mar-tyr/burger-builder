66eddc948695b8932aaf3af58e61da64080d7d87
Can <canx@andrew.cmu.edu>
2019-12-05 22:00:18 -0800

    Add: config burger ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,17 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from './Burger.module.css'
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((ingredient) => {
+    return [...Array(props.ingredients[ingredient])].map((_, i) => {
+      return <BurgerIngredient type={ingredient} key={ingredient + "_" + i}/>
+    });
+  });
+
   return (
     <div className={classes.Burger}>
-      <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="meat"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="bread-bottom"/>
+      <BurgerIngredient type='bread-top'/>
+      {transformedIngredients}
+      <BurgerIngredient type='bread-bottom'/>
     </div>
   )
 };
