2aad0316298cd1b49cc4a5360bbb03d2b146eb06
Can <canx@andrew.cmu.edu>
2019-12-16 13:57:41 -0800

    Modify: optimize the modal update

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 import PropsType from 'prop-types';
 
-const Modal = (props) => {
-  return (
-    <Aux>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0',
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 Modal.propsType = {
   show: PropsType.bool.isRequired,
